ec9d41269c68b1089460fab3ea6914e201b75461
ean <torrescharles0307@gmail.com>
2024-04-27 10:11:55 +0800

    tailwind,destructture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,2 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+import { Routes } from '@angular/router';
+export const routes: Routes = [];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+@Component({ 
+  selector: 'app-root', 
+  standalone: true, 
+  imports: [RouterOutlet,ReactiveFormsModule,HttpClientModule], 
+  templateUrl: './app.component.html', 
+  styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent { 
   title = 'angular-test';
 }
