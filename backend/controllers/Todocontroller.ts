d8158a2f06387f015c9bc3895d42ec25a3d43673
ean <torrescharles0307@gmail.com>
2024-04-30 23:37:59 +0800

    working create backend

---
+++ backend/controllers/Todocontroller.ts
@@ -0,0 +1,25 @@
+const { PrismaClient } = require('@prisma/client');
+import {Request, Response} from 'express';
+
+const prisma = new PrismaClient();
+
+async function CreateList(req: Request, res: Response) {
+    const { owner, task } = req.body
+    
+    const result = await prisma.todo.create({
+        data: {
+              owner,
+              task
+        },
+    }) 
+    if (!result){
+        res.status(500).json({message: 'Error creating todo', result})
+    }
+    else{
+        res.status(200).json(result)
+    }
+  }
+
+  const TodoController = {CreateList};
+
+  export default TodoController;
--- backend/index.ts
+++ backend/index.ts
@@ -1,61 +1,16 @@
 import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import TodoRouter from './routes/Todo';
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-
-const prisma = new PrismaClient()
+const PORT = 8080;
 const app = express()
+
+
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/task', async (req, res) => {
-  try {
-    const { owner, task } = req.body
-    const item = await prisma.todo.create({
-      data: {
-        owner,
-        task
-      },
-    })
-    res.json(item)
-  } catch (error) {
-    console.error('Error adding item:', error)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-})
+app.use('/todo',TodoRouter);
 
-// app.delete('/deleteitem/:id', async(req,res) =>{
-//   const itemid = req.params.id;
-//   const deleteItem = await prisma.item.delete({
-//     where:{
-//       id: itemid
-//     }
-//   })
-//   console.log("Item is deleted" + deleteItem)
-// })
-
-// app.put('/updateitem/:id', async(req,res) =>{
-//   const itemid = req.params.id;
-//   const { name } = req.body;
-//   const deleteItem = await prisma.item.update({
-//     where:{
-//       id: itemid
-//     },
-//     data:{
-//       name
-//     },
-//   })
-//   console.log("Item is deleted" + deleteItem)
-// })
-
-// app.get('/display', async (req, res) => {
-//   const items = await prisma.item.findMany();
-//   res.json(items);
-// });
-
-
-// const server = app.listen(3000, () => {
-//   console.log('Server is running on port 3000')
-// })
-
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
+});
