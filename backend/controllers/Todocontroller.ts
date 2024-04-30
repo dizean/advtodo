b8a14d93be3c163774b38166972053fa78754f90
ean <torrescharles0307@gmail.com>
2024-05-01 01:32:49 +0800

    salamat naman ,control expects array the maps to get fields

--- backend/controllers/Todocontroller.ts
+++ backend/controllers/Todocontroller.ts
@@ -4,19 +4,26 @@ import { Request, Response } from 'express';
 const prisma = new PrismaClient();
 
 async function CreateList(req: Request, res: Response) {
-    const { owner, task } = req.body;
-    const result = await prisma.todo.create({
-        data: {
-            owner: owner,
-            task: task
-        },
-    });
-    if (!result) {
-        res.status(500).json({ message: 'Error creating todo', result });
-    } else {
-        res.status(200).json(result);
+    try {
+        const todos = req.body;
+
+        const createdTodos = await Promise.all(todos.map(async (todo: any) => {
+            const result = await prisma.todo.create({
+                data: {
+                    owner: todo.owner,
+                    task: todo.task
+                },
+            });
+            return result;
+        }));
+
+        res.status(200).json(createdTodos);
+    } catch (error) {
+        console.error('Error creating todos:', error);
+        res.status(500).json({ message: 'Error creating todos', error });
     }
 }
+
 
 const TodoController = { CreateList };
 
