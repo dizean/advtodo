b47e021d3e8a28792524374f72f86fee53aaf6de
ean <torrescharles0307@gmail.com>
2024-05-05 11:49:34 +0800

    testpage branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ToDOComponent } from './to-do/to-do.component';
+import { AccountComponent } from './account/account.component';
 export const routes: Routes = 
 [
     {path:'todo', component: ToDOComponent},
+    {path:'account', component: AccountComponent},
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { RouterOutlet } from '@angular/router';
-import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ToDOComponent } from './to-do/to-do.component';
+import { AccountComponent } from './account/account.component';
 @Component({ 
   selector: 'app-root', 
   standalone: true, 
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     ReactiveFormsModule,
     HttpClientModule,
-  ToDOComponent], 
+    ToDOComponent,
+  AccountComponent], 
   templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
